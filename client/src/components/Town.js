3919def250c9fbc791787a2d0ada6664e715008a
Son <sonbui.inbox@gmail.com>
2019-03-11 10:28:29 -0400

    wireframing (update 2)

--- client/src/components/Town.js
+++ client/src/components/Town.js
@@ -4,10 +4,12 @@ import '../App.css'
 const Town = (props) => (
     <div class="container">
         <h2>Town</h2>
-        <button type="button" class="btn btn-primary" onClick={props.handleShopButton}>Shop</button>
-        <button type="button" class="btn btn-primary" onClick={props.handleForestButton}>Forest</button>
-        <button type="button" class="btn btn-primary" onClick={props.handleMountainButton}>Mountain</button>
-        <div id="inventory"></div>
+        <button type="button" id="townShopBtn" class="btn btn-primary" onClick={props.handleShopButton}>Shop</button>
+        <button type="button" id="townForestBtn" class="btn btn-primary" onClick={props.handleForestButton}>Forest</button>
+        <button type="button" id="townMountainBtn" class="btn btn-primary" onClick={props.handleMountainButton}>Mountain</button>
+        <div id="inventory">
+            <h2>Inventory</h2>
+        </div>
         
         <div id="gameTextBox">
             <br />
