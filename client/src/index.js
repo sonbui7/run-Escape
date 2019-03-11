1b245e3f0a623afbb144a250719f4446ec8bb387
Kori <icedragon668@gmail.com>
2019-03-11 09:55:48 -0400

    battle componet works

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';                                     //this was changed to render battle
+import App from './App';                                     //this was changed to render battle
 import * as serviceWorker from './serviceWorker';
 
-import Battle from "./components/Battle"                        //this was changed to render battle
-ReactDOM.render(<Battle />, document.getElementById('root'));   //this was changed to render battle
+// import Battle from "./components/Battle"                        //this was changed to render battle
+// ReactDOM.render(<Battle />, document.getElementById('root'));   //this was changed to render battle
 
-// ReactDOM.render(<App />, document.getElementById('root'));   //this was changed to render battle
+ReactDOM.render(<App />, document.getElementById('root'));   //this was changed to render battle
 
 serviceWorker.unregister();
