88b428f6a04097cca82ff453947943aa08c70c24
Haoyuan Lin <specialemon@gmail.com>
2019-03-12 21:55:22 -0400

    effects

--- client/src/components/InventoryUi/Armor.js
+++ client/src/components/InventoryUi/Armor.js
@@ -11,7 +11,7 @@ const Armor = (props) => {
                     <div className="row itemRow">
                         <div className="col col-3 itemName">{armor.itemName}</div>
                         <div className="col col-2 itemAmount">X {armor.amount}</div>
-                        <div className="col col-6 itemDescription"><p>{armor.itemProperties.description}</p></div>
+                        <div className="col col-6 itemDescription"><p>{armor.itemProperties.description}</p><p>+ {armor.itemProperties.effect} Max Hp</p></div>
                         <button val={JSON.stringify(armor)} onClick={props.handleArmor}>Equip</button>
                     </div>
                     <hr></hr>
